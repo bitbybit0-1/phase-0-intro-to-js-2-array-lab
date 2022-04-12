c9b9d1eab639502dc737621af74fc0f3e672ffd5
joseph sutera <joesutera11@gmail.com>
2022-04-12 11:19:28 -0400

    pushing code

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+let cats =["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+function appendCat(name){
+    cats.splice(name);
+    return cats
+    
+}
+function prependCat(name){
+    cats.unshift(name);
+    return cats
+}
+function removesLastCat(name){
+    cats.pop(name);
+    return cats
+}
