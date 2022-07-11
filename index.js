d2cb95b7f8d79dd61c9b11b400404c5e19485703
Joseph Sutera <joesutera11@gmail.com>
2022-07-11 19:56:51 -0400

    commit tree bf008be1959422135196174bb800afd8a98a355d

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 // Write your solution here!
 let cats =["Milo", "Otis", "Garfield"];
 
+function appendCat(name){
+let copyOfCats=[...cats,name];
+console.log(copyOfCats);
+return copyOfCats
+}
+function prependCat(name){
+   let newCats=[name, ...cats];
+   console.log(newCats);
+    return newCats
+}
+
 function destructivelyAppendCat(name){
-cats.push(name);
+ cats.push(name);
+
 }
 function destructivelyPrependCat(name){
     cats.unshift(name);
@@ -13,16 +25,22 @@ function destructivelyRemoveLastCat(name){
 function destructivelyRemoveFirstCat(name){
     cats.shift(name);
 }
-function appendCat(name){
-    cats.splice(name);
-    return cats
-    
+function removeLastCat(){
+   return  cats.slice(0,cats.length-1);
 }
-function prependCat(name){
-    cats.unshift(name);
+function  removeFirstCat(){
+    return cats.slice(1,cats.length)
+}
+//function appendCat(name){
+  //  cats.splice(name);
+  //  return cats
+    
+//          
+//function prependCat(name){
+   /* cats.unshif(name);
     return cats
 }
 function removesLastCat(name){
     cats.pop(name);
     return cats
-}
+*/
